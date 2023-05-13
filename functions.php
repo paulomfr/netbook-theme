418c8db857737632b27f747fe53f30d7af6b9bcc
Paulo Ferreira <hi@paulofr.dev>
2023-05-13 16:22:07 -0300

    Created the header part

--- functions.php
+++ functions.php
@@ -17,8 +17,57 @@
 function netbook_setup()
 {
   add_theme_support( 'wp-block-styles' );
+  // add_editor_style( 'dist/editor.css' );
 }
 add_action( 'after_setup_theme', 'netbook_setup' );
+
+/**
+ * Assets.
+ */
+
+// Editor.
+function enqueue_editor()
+{
+    $template_directory = get_template_directory_uri();
+
+    // Register scripts.
+    wp_register_script(
+        'netbook.scripts.editor',
+        $template_directory . '/dist/editor.js',
+    );
+
+    wp_register_style(
+        'netbook.styles.editor',
+        $template_directory . '/dist/editor.css'
+    );
+
+    // Enqueue scripts.
+    wp_enqueue_script( 'netbook.scripts.editor' );
+    wp_enqueue_style( 'netbook.styles.editor' );
+}
+add_action( 'enqueue_block_editor_assets', 'enqueue_editor' );
+
+// Frontned.
+// function enqueue_frontend()
+// {
+//     $template_directory = get_template_directory_uri();
+
+//     // Register scripts.
+//     wp_register_script(
+//         'netbook.scripts.frontend',
+//         $template_directory . '/dist/editor.js',
+//     );
+
+//     wp_register_style(
+//         'netbook.styles.frontend',
+//         $template_directory . '/dist/editor.css'
+//     );
+
+//     // Enqueue scripts.
+//     wp_enqueue_script( 'netbook.scripts.frontend' );
+//     wp_enqueue_style( 'netbook.styles.frontend' );
+// }
+// add_action( 'wp_enqueue_scripts', 'enqueue_frontend' );
 
 /**
  * Registers navigation menus.
---
+++ patterns/header.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Title: Header
+ * Slug: netbook/header
+ * Categories: netbook-patterns, header
+ */
+?>
+<<!-- wp:group {"layout":{"type":"constrained"}} -->
+<div class="wp-block-group"><!-- wp:group {"align":"wide","style":{"spacing":{"padding":{"top":"0.94rem","bottom":"0.94rem"},"blockGap":"0"}},"layout":{"type":"flex","flexWrap":"nowrap","justifyContent":"space-between"}} -->
+<div class="wp-block-group alignwide" style="padding-top:0.94rem;padding-bottom:0.94rem"><!-- wp:group {"style":{"spacing":{"blockGap":"72px"}},"layout":{"type":"flex","flexWrap":"nowrap"}} -->
+<div class="wp-block-group"><!-- wp:site-logo {"width":143,"shouldSyncIcon":true} /-->
+
+<!-- wp:navigation {"ref":15,"style":{"spacing":{"blockGap":"36px"}}} /--></div>
+<!-- /wp:group -->
+
+<!-- wp:group {"layout":{"type":"flex","flexWrap":"nowrap"}} -->
+<div class="wp-block-group"><!-- wp:buttons -->
+<div class="wp-block-buttons"><!-- wp:button {"style":{"typography":{"fontSize":"12px"},"spacing":{"padding":{"top":"0.88rem","bottom":"0.88rem","left":"2.69rem","right":"2.69rem"}}}} -->
+<div class="wp-block-button has-custom-font-size" style="font-size:12px"><a class="wp-block-button__link wp-element-button" style="padding-top:0.88rem;padding-right:2.69rem;padding-bottom:0.88rem;padding-left:2.69rem"><strong>Log in</strong></a></div>
+<!-- /wp:button --></div>
+<!-- /wp:buttons --></div>
+<!-- /wp:group --></div>
+<!-- /wp:group --></div>
+<!-- /wp:group -->
